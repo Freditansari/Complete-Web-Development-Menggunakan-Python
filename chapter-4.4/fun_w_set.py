41bf8d526f407917fe2bd5c8fd5e30129708b117
Freditansari <Freditansari@users.noreply.github.com>
2021-09-08 14:14:22 +0700

    added set example

---
+++ chapter-4.4/fun_w_set.py
@@ -0,0 +1,12 @@
+set_category = {'fictions', 'non-fictions', 'textbook', 'biography'}
+
+set_category.add('history')
+
+set_category.remove('biography')
+print(set_category)
+
+#https://www.w3schools.com/python/python_sets_methods.asp
+myList = [1,1,2,3,1,3,4,5,6,6]
+
+unique_list = set(myList)
+print(unique_list)
