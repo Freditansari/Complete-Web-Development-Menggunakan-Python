16c9952eb03dc7c459467c83bf5c75d00fe4f25d
Freditansari <Freditansari@users.noreply.github.com>
2021-09-09 12:29:40 +0700

    added tuple example

---
+++ chapter-5/fun_with_if_else.py
@@ -0,0 +1,14 @@
+kondisi_a = 10
+kondisi_b = 100
+
+kondisi_c = 90
+kondisi_d = 30
+
+if kondisi_a> kondisi_b:
+    if kondisi_c <= kondisi_d:
+        print("Z")
+    else:
+        print('26')
+else:
+    print('B')
+
