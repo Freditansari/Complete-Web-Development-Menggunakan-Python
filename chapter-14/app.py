83156cdf4c40bbe55a8750a143b254577c8efe57
Freditansari <Freditansari@users.noreply.github.com>
2021-09-17 14:27:30 +0700

    added routing examples

---
+++ chapter-14/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return "<h1> Hello world</h1>"
+
+
+@app.route('/hello/<string:name>', methods=['GET'])
+def say_hello(name):
+    return f'Hello {name[99]}, welcome to my basic website.'
+
+
