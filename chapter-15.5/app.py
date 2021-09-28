d05df343a954abab605fea92a888d079cdf3a89b
Freditansari <Freditansari@users.noreply.github.com>
2021-09-28 14:35:10 +0700

    added layout example

---
+++ chapter-15.5/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# @app.route('/')
+# def hello_world():
+#     return "<h1> Hello world</h1>"
+
+@app.route('/')
+def blog_home():
+    headline = 'World Peace Achieved!'
+    lead = 'abc laughed a lot and all the world leaders grown a sense of humor'
+    customer_lists = [{'name': 'John', 'upvote': 20, 'downvote': 10},
+                      {'name': 'Banana', 'upvote': 50, 'downvote': 10},
+                      {'name': 'Susan', 'upvote': 15, 'downvote': 17}]
+    return render_template('home.html', headline=headline, lead=lead, is_true = True, customers = customer_lists)
+
+@app.route('/aboutme')
+def aboutme():
+    return render_template('about_us.html')
+
+
+
+
+if __name__ == 'main':
+    app.run(debug=True)
